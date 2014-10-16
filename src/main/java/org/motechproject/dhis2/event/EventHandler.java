dc7fe44bba922d7300b5fa75c6006df9d2479c17
Scott Kimball <scott.r.kimball@maine.edu>
2014-10-16 10:58:27 -0400

    Deleted custom parser in event handler. Deleted unnecessary task actions in task channel.

--- src/main/java/org/motechproject/dhis2/event/EventHandler.java
+++ src/main/java/org/motechproject/dhis2/event/EventHandler.java
@@ -33,16 +33,12 @@ public class EventHandler {
     private String nationalIdentifierUUID = "AuPLng5hLbE";
     private String stage_uuid = "vxQUcroMY0r";
 
-
-
-
     private SendAggregateDataService sendAggregateDataService;
     private Logger logger = LoggerFactory.getLogger(EventHandler.class);
     private EventRelay eventRelay;
     private EnrollmentService enrollmentService;
     private ProgramDataService programDataService;
     private StageService stageService;
-
 
 
     @Autowired
@@ -60,26 +56,6 @@ public class EventHandler {
 
     }
 
-    @MotechListener(subjects = {EventSubjects.ENROLL_IN_PROGRAM})
-    public void handleEnrollment (MotechEvent event) {
-
-        logger.debug("Recieved enrollment event :" );
-        logger.debug(event.toString());
-
-       MotechEvent newEvent = parseEnrollmentData(event);
-        Enrollment enrollment = buildEnrollment(newEvent);
-        enrollmentService.send(enrollment);
-
-    }
-
-    @MotechListener(subjects = {EventSubjects.SEND_AGGREGATE_DATA})
-    public void handleAggregateData (MotechEvent event) throws Exception{
-        logger.debug("Recieved Aggregate Data Event");
-        logger.debug("Event details: " + event);
-
-        DataValue dataValue = parseAggregateData(event);
-        sendAggregateDataService.send(dataValue);
-    }
 
     @MotechListener(subjects  = {EventSubjects.TB_REGISTRATION})
     public void handleTbRegistration (MotechEvent event) {
@@ -118,120 +94,6 @@ public class EventHandler {
         }
 
 
-    private DataValue parseAggregateData (MotechEvent event) {
-        Map<String, Object> eventParameters =  event.getParameters();
-        Map<String, Map> params = (Map<String, Map>) eventParameters.get(EventParams.PARAMS);
-        String s = params.toString();
-
-        Scanner sc = new Scanner (s).useDelimiter("[^a-zA-Z0-9]");
-        String value = null;
-        String orgUnit = null;
-        String timePeriod = null;
-        boolean gotValue = false;
-        boolean gotOrgUnit = false;
-        boolean gotTimePeriod = false;
-
-        while (sc.hasNext() && !gotTimePeriod ) {
-            String temp = sc.next();
-
-            if (temp.equals(EventParams.NAME) && !gotValue ) {
-
-                do {
-                   temp = sc.next();
-                    } while (!temp.equals(EventParams.VALUE));
-
-                value = sc.next();
-                gotValue = true;
-
-
-            } else if (temp.equals(EventParams.ORG_UNIT) && !gotOrgUnit ) {
-
-                do {
-                    temp = sc.next();
-                } while (!temp.equals(EventParams.VALUE));
-
-                orgUnit =sc.next();
-                gotOrgUnit = true;
-
-            } else if (temp.equals(EventParams.TIME_PERIOD)) {
-
-                do {
-                    temp = sc.next();
-                } while (!temp.equals(EventParams.VALUE));
-
-                timePeriod = sc.next()  + sc.next() +  sc.next();
-                gotTimePeriod = true;
-
-            }
-        }
-
-
-     //  DataValue dataValue = dataValueService.create(EventParams.NAME, value, orgUnit, timePeriod);
-
-       return null;
-    }
-
-    /*
-    Parses the malformed event and creates new correctly formed event. Once the Commcare / Tasks issue has been
-    resolved, this method can be removed.
-     */
-    private MotechEvent parseEnrollmentData(MotechEvent event) {
-
-        Map<String, Object> eventParameters =  event.getParameters();
-        Map<String, Map> params = (Map<String, Map>) eventParameters.get("subElements");
-        String s = params.toString();
-
-        Scanner sc = new Scanner (s).useDelimiter("[^a-zA-Z0-9&&[^_-]]");
-        String lastName = null,firstName = null, gender = null,
-                location = null, nationalIdentifier = null, caseId = null,
-                caseType = null, dateRegistered = null;
-
-        while (sc.hasNext() && caseType == null ) {
-            String temp = sc.next();
-
-            if (temp.equals(EventParams.LAST_NAME) && lastName == null ) {
-                lastName = findValue(sc);
-
-            } else if (temp.equals(EventParams.FIRST_NAME) && firstName == null ) {
-                firstName = findValue(sc);
-
-            } else if (temp.equals(EventParams.GENDER) && gender == null) {
-                gender = findValue(sc);
-
-            } else if (temp.equals(EventParams.LOCATION) && location == null) {
-                location = findValue(sc);
-
-            } else if (temp.equals(EventParams.NATIONAL_IDENTIFIER) && nationalIdentifier == null) {
-                nationalIdentifier = findValue(sc);
-
-            } else if (temp.equals(EventParams.DATE_REGISTERED) && dateRegistered == null) {
-                dateRegistered = findValue(sc);
-
-            } else if (temp.equals(EventParams.CASE_ID) && caseId == null) {
-                caseId = sc.next();
-
-            } else if (temp.equals(EventParams.CASE_TYPE)) {
-                caseType = findValue(sc);
-            }
-        }
-
-
-        // builds new event that looks more like a correctly formed event
-        Map <String , Object> paramMap = new HashMap<String, Object>();
-        paramMap.put(EventParams.LAST_NAME , lastName);
-        paramMap.put(EventParams.FIRST_NAME, firstName);
-        paramMap.put(EventParams.GENDER, gender);
-        paramMap.put(EventParams.LOCATION,location);
-        paramMap.put(EventParams.NATIONAL_IDENTIFIER,nationalIdentifier);
-        paramMap.put(EventParams.DATE_REGISTERED,dateRegistered);
-        paramMap.put(EventParams.CASE_ID,caseId);
-        paramMap.put(EventParams.CASE_TYPE,caseType);
-
-        MotechEvent newEvent = new MotechEvent(EventSubjects.ENROLL_IN_PROGRAM , paramMap);
-
-        return newEvent;
-    }
-
 
     /* Ths method builds an enrollment object from a motech event. Right now, it builds new entities on the fly
      * but eventually the following sequence will occur:
@@ -260,9 +122,11 @@ public class EventHandler {
 
         //Required Attribute for program
         Attribute programRequiredAttribute = new Attribute("nationalIdentifier",nationalIdentifierUUID,null);
+
         // list for programattribute list
         List<Attribute> programAttributeList = new ArrayList<Attribute>();
         programAttributeList.add(programRequiredAttribute);
+
         // create program
         Program program = new Program((String) params.get(EventParams.CASE_TYPE) ,"TB Visit" ,programUUID,
                 trackedEntity , programAttributeList);
@@ -291,17 +155,6 @@ public class EventHandler {
         Enrollment enrollment = new Enrollment(program,trackedEntityInstance,orgUnit,date );
 
         return enrollment;
-    }
-
-
-    private String findValue (Scanner scanner ) {
-        String temp;
-        do {
-            temp = scanner.next();
-        } while (!temp.equals(EventParams.VALUE));
-
-        return scanner.next();
-
     }
 
 }
