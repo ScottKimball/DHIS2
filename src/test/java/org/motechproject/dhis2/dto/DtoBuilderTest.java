22c4a6cf82f6b9a75ab1daa5b14028516e6d99ab
Scott-K <scott.russell.kimball@gmail.com>
2015-01-27 15:20:38 -0500

    added stage update test

--- src/test/java/org/motechproject/dhis2/dto/DtoBuilderTest.java
+++ src/test/java/org/motechproject/dhis2/dto/DtoBuilderTest.java
@@ -10,6 +10,7 @@ import org.motechproject.dhis2.domain.TrackedEntityInstanceMapper;
 import org.motechproject.dhis2.dto.builder.DtoBuilder;
 import org.motechproject.dhis2.dto.impl.AttributeDto;
 import org.motechproject.dhis2.dto.impl.EnrollmentDto;
+import org.motechproject.dhis2.dto.impl.StageDto;
 import org.motechproject.dhis2.dto.impl.TrackedEntityInstanceDto;
 import org.motechproject.dhis2.event.EventParams;
 import org.motechproject.dhis2.event.EventSubjects;
@@ -22,7 +23,6 @@ import java.util.List;
 import java.util.Map;
 
 import static junit.framework.Assert.*;
-import static org.mockito.Matchers.endsWith;
 import static org.mockito.Mockito.when;
 
 /**
@@ -31,6 +31,7 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DtoBuilderTest {
+
     private static final String ENTITY_TYPE_ID = "entityUUID"; // Person
     private static final String INSTANCE_EXT_ID = "externalId";
     private static final String INSTANCE_DHIS2_ID = "dhis2ID";
@@ -38,6 +39,7 @@ public class DtoBuilderTest {
     private static final String ORGUNIT_ID = "OrgUnitID";
     private static final String PROGRAM_ID = "programID";
     private static final String DATE = "date";
+    private static final String STAGE_ID ="stageID";
 
 
     private AttributeDto testAttributeDto1;
@@ -59,13 +61,10 @@ public class DtoBuilderTest {
 
         orgUnit = new OrgUnit(ORGUNIT_NAME,ORGUNIT_ID);
         mapper = new TrackedEntityInstanceMapper(INSTANCE_EXT_ID,INSTANCE_DHIS2_ID);
+        builder = new DtoBuilder(trackedEntityInstanceDataService,orgUnitDataService);
 
         testAttributeDto1 = new AttributeDto(null, "attributeUuid1", "attributeValue1");
         testAttributeDto2 = new AttributeDto(null,"attributeUuid2","attributeValue2");
-
-
-        builder = new DtoBuilder(trackedEntityInstanceDataService,orgUnitDataService);
-
 
         when(orgUnitDataService.findByName(ORGUNIT_NAME)).thenReturn(orgUnit);
         when(trackedEntityInstanceDataService.findByExternalName(INSTANCE_EXT_ID)).thenReturn(mapper);
@@ -85,8 +84,8 @@ public class DtoBuilderTest {
         params.put(EventParams.ENTITY_TYPE, ENTITY_TYPE_ID);
         params.put(EventParams.EXTERNAL_ID, INSTANCE_EXT_ID);
         params.put(EventParams.LOCATION, ORGUNIT_NAME);
-        params.put("attributeUuid1", "attributeValue1");
-        params.put("attributeUuid2", "attributeValue2");
+        params.put(testAttributeDto1.getDhis2Uuid(), testAttributeDto1.getValue());
+        params.put(testAttributeDto2.getDhis2Uuid(), testAttributeDto2.getValue());
         params.put("nullValue", null);
 
         MotechEvent event = new MotechEvent(EventSubjects.REGISTER_ENTITY,params);
@@ -105,8 +104,6 @@ public class DtoBuilderTest {
         assertTrue(attributeDtos.contains(testAttributeDto2));
 
         assertEquals(instance.toJson(), toJson);
-
-
     }
 
     @Test
@@ -122,8 +119,8 @@ public class DtoBuilderTest {
         params.put(EventParams.PROGRAM,PROGRAM_ID);
         params.put(EventParams.EXTERNAL_ID,INSTANCE_EXT_ID);
         params.put(EventParams.DATE,DATE);
-        params.put("attributeUuid1", "attributeValue1");
-        params.put("attributeUuid2", "attributeValue2");
+        params.put(testAttributeDto1.getDhis2Uuid(), testAttributeDto1.getValue());
+        params.put(testAttributeDto2.getDhis2Uuid(), testAttributeDto2.getValue());
         params.put("nullValue", null);
 
         MotechEvent event = new MotechEvent(EventSubjects.ENROLL_IN_PROGRAM,params);
@@ -144,8 +141,46 @@ public class DtoBuilderTest {
         assertEquals(toJson,enrollmentDto.toJson());
 
 
-
     }
 
-    
+    @Test
+    public void testStageUpdate() throws Exception {
+
+        String toJson = "{\"program\":\"programID\",\"orgUnit\":\"OrgUnitID\",\"eventDate\":\"date\"," +
+                "\"programStage\":\"stageID\",\"trackedEntityInstance\":\"dhis2ID\",\"dataValues\":" +
+                "[{\"dataElement\":\"attributeUuid2\",\"value\":\"attributeValue2\"}," +
+                "{\"dataElement\":\"attributeUuid1\",\"value\":\"attributeValue1\"}]}";
+
+
+        Map<String, Object> params = new HashMap<>();
+        params.put(EventParams.EXTERNAL_ID,INSTANCE_EXT_ID);
+        params.put(EventParams.LOCATION,ORGUNIT_NAME);
+        params.put(EventParams.DATE,DATE);
+        params.put(EventParams.STAGE,STAGE_ID);
+        params.put(EventParams.PROGRAM,PROGRAM_ID);
+        params.put(testAttributeDto1.getDhis2Uuid(), testAttributeDto1.getValue());
+        params.put(testAttributeDto2.getDhis2Uuid(), testAttributeDto2.getValue());
+        params.put("nullValue", null);
+
+        MotechEvent event = new MotechEvent(EventSubjects.UPDATE_PROGRAM_STAGE, params);
+
+        StageDto stageDto = (StageDto) builder.createDto(event);
+
+        assertNotNull(stageDto);
+        assertEquals(stageDto.getTrackedEntityInstance(),INSTANCE_DHIS2_ID);
+        assertEquals(stageDto.getDate(),DATE);
+        assertEquals(stageDto.getProgram(),PROGRAM_ID);
+        assertEquals(stageDto.getOrgUnit(),ORGUNIT_ID);
+        assertEquals(stageDto.getDhis2Uuid(),STAGE_ID);
+        assertEquals(stageDto.getTrackedEntityInstance(),INSTANCE_DHIS2_ID);
+
+        List<AttributeDto> dataElements = stageDto.getDataElementDtos();
+        assertNotNull(dataElements);
+        assertTrue(dataElements.contains(testAttributeDto1));
+        assertTrue(dataElements.contains(testAttributeDto2));
+        assertEquals(2,dataElements.size());
+
+        assertEquals(toJson,stageDto.toJson());
+
+    }
 }
