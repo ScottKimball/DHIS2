f5ad3c566547dc6ea7a5cc27077341848044ce9e
Scott-K <scott.russell.kimball@gmail.com>
2015-01-27 14:43:59 -0500

    Added Program enrollment test

--- src/test/java/org/motechproject/dhis2/dto/DtoBuilderTest.java
+++ src/test/java/org/motechproject/dhis2/dto/DtoBuilderTest.java
@@ -9,6 +9,7 @@ import org.motechproject.dhis2.domain.OrgUnit;
 import org.motechproject.dhis2.domain.TrackedEntityInstanceMapper;
 import org.motechproject.dhis2.dto.builder.DtoBuilder;
 import org.motechproject.dhis2.dto.impl.AttributeDto;
+import org.motechproject.dhis2.dto.impl.EnrollmentDto;
 import org.motechproject.dhis2.dto.impl.TrackedEntityInstanceDto;
 import org.motechproject.dhis2.event.EventParams;
 import org.motechproject.dhis2.event.EventSubjects;
@@ -21,6 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 import static junit.framework.Assert.*;
+import static org.mockito.Matchers.endsWith;
 import static org.mockito.Mockito.when;
 
 /**
@@ -34,6 +36,15 @@ public class DtoBuilderTest {
     private static final String INSTANCE_DHIS2_ID = "dhis2ID";
     private static final String ORGUNIT_NAME = "OrgUnitName";
     private static final String ORGUNIT_ID = "OrgUnitID";
+    private static final String PROGRAM_ID = "programID";
+    private static final String DATE = "date";
+
+
+    private AttributeDto testAttributeDto1;
+    private AttributeDto testAttributeDto2;
+    private DtoBuilder builder;
+    private OrgUnit orgUnit;
+    private TrackedEntityInstanceMapper mapper;
 
 
     @Mock
@@ -42,15 +53,16 @@ public class DtoBuilderTest {
     @Mock
     private OrgUnitDataService orgUnitDataService;
 
-    DtoBuilder builder;
-    OrgUnit orgUnit;
-    TrackedEntityInstanceMapper mapper;
 
     @Before
     public void setup() {
 
         orgUnit = new OrgUnit(ORGUNIT_NAME,ORGUNIT_ID);
-        mapper = new TrackedEntityInstanceMapper(INSTANCE_EXT_ID,null);
+        mapper = new TrackedEntityInstanceMapper(INSTANCE_EXT_ID,INSTANCE_DHIS2_ID);
+
+        testAttributeDto1 = new AttributeDto(null, "attributeUuid1", "attributeValue1");
+        testAttributeDto2 = new AttributeDto(null,"attributeUuid2","attributeValue2");
+
 
         builder = new DtoBuilder(trackedEntityInstanceDataService,orgUnitDataService);
 
@@ -67,8 +79,7 @@ public class DtoBuilderTest {
                 "[{\"attribute\":\"attributeUuid2\",\"value\":\"attributeValue2\"},{\"attribute\":" +
                 "\"attributeUuid1\",\"value\":\"attributeValue1\"}]}";
 
-        AttributeDto testAttributeDto1 = new AttributeDto(null, "attributeUuid1", "attributeValue1");
-        AttributeDto testAttributeDto2 = new AttributeDto(null,"attributeUuid2","attributeValue2");
+
 
         Map<String, Object> params = new HashMap<>();
         params.put(EventParams.ENTITY_TYPE, ENTITY_TYPE_ID);
@@ -82,7 +93,6 @@ public class DtoBuilderTest {
 
         TrackedEntityInstanceDto instance = (TrackedEntityInstanceDto) builder.createDto(event);
 
-
         assertNotNull(instance);
         assertEquals(instance.getTrackedEntityType(),ENTITY_TYPE_ID);
         assertEquals(instance.getExternalId(),INSTANCE_EXT_ID);
@@ -93,9 +103,49 @@ public class DtoBuilderTest {
         assertEquals(2,attributeDtos.size());
         assertTrue(attributeDtos.contains(testAttributeDto1));
         assertTrue(attributeDtos.contains(testAttributeDto2));
-        
+
         assertEquals(instance.toJson(), toJson);
 
 
     }
+
+    @Test
+    public void testProgramEnrollment() throws Exception {
+
+        String toJson = "{\"program\":\"programID\",\"trackedEntityInstance\":\"dhis2ID\"," +
+                "\"dateOfEnrollment\":\"date\",\"attributes\":[{\"attribute\":\"attributeUuid2\"," +
+                "\"value\":\"attributeValue2\"},{\"attribute\":\"attributeUuid1\",\"value\":\"attributeValue1\"}]}";
+
+
+
+        Map<String, Object> params = new HashMap<>();
+        params.put(EventParams.PROGRAM,PROGRAM_ID);
+        params.put(EventParams.EXTERNAL_ID,INSTANCE_EXT_ID);
+        params.put(EventParams.DATE,DATE);
+        params.put("attributeUuid1", "attributeValue1");
+        params.put("attributeUuid2", "attributeValue2");
+        params.put("nullValue", null);
+
+        MotechEvent event = new MotechEvent(EventSubjects.ENROLL_IN_PROGRAM,params);
+
+        EnrollmentDto enrollmentDto = (EnrollmentDto) builder.createDto(event);
+        assertNotNull(enrollmentDto);
+        assertEquals(enrollmentDto.getProgram(),PROGRAM_ID);
+        assertEquals(enrollmentDto.getDate(),DATE);
+        assertEquals(enrollmentDto.getTrackedEntityInstance(),INSTANCE_DHIS2_ID);
+
+        List<AttributeDto> attributeDtos = enrollmentDto.getAttributeDtos();
+
+        assertNotNull(attributeDtos);
+        assertTrue(attributeDtos.contains(testAttributeDto1));
+        assertTrue(attributeDtos.contains(testAttributeDto2));
+        assertEquals(2,attributeDtos.size());
+
+        assertEquals(toJson,enrollmentDto.toJson());
+
+
+
+    }
+
+    
 }
